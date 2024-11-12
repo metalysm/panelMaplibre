ab43eaa6e55c702072263ef9dcaba85142b8be82
Burak Demir <burakdemir400@gmail.com>
2024-11-12 14:21:05 +0300

    arcgis_layer_connection

--- map_scripts.js
+++ map_scripts.js
@@ -1,27 +1,194 @@
-import mapboxgl from 'mapbox-gl';
-import FeatureService from 'mapbox-gl-arcgis-featureserver';
+import maplibregl from 'https://cdn.jsdelivr.net/npm/maplibre-gl@4.7.1/+esm';
+import mapboxGlArcgisFeatureserver from 'https://cdn.jsdelivr.net/npm/mapbox-gl-arcgis-featureserver@0.0.8/+esm';
 
-const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'https://api.maptiler.com/maps/basic-v2/style.json?key=ka2CI0XYdBCZt32lmrGA',
-  center: [28.06, 41.77], 
-  zoom: 10
-});
-
-map.on('load', () => {
-  const fsSourceId = 'featureserver-src';
-
-  const service = new FeatureService(fsSourceId, map, {
-    url: 'https://portal.spatial.nsw.gov.au/server/rest/services/NSW_Administrative_Boundaries_Theme/FeatureServer/6'
+export function render({ model, el }) {
+  const map = new maplibregl.Map({
+    container: el,
+    style: model.tile_url,
+    center: model.center,
+    zoom: model.zoom,
+    pitch: model.pitch,
+    bearing: model.bearing,
+    antialias: true
   });
 
-  map.addLayer({
-    'id': 'fill-lyr',
-    'source': fsSourceId,
-    'type': 'fill',
-    'paint': {
-      'fill-opacity': 0.5,
-      'fill-color': '#B42222'
-    }
+  const controlsContainer = document.createElement('div');
+  controlsContainer.style.position = 'absolute';
+  controlsContainer.style.top = '10px';
+  controlsContainer.style.left = '10px';
+  controlsContainer.style.zIndex = '1';
+  controlsContainer.style.backgroundColor = 'white';
+  controlsContainer.style.padding = '2px';
+  controlsContainer.style.borderRadius = '3px';
+
+  const firstLayerButton = document.createElement('button');
+  firstLayerButton.textContent = 'İlk Katman';
+  firstLayerButton.onclick = () => {
+    model.show_first_layer = !model.show_first_layer;
+  };
+  controlsContainer.appendChild(firstLayerButton);
+
+  const secondLayerButton = document.createElement('button');
+  secondLayerButton.textContent = 'İlçe Sınırları';
+  secondLayerButton.onclick = () => {
+    model.show_second_layer = !model.show_second_layer;
+  };
+  controlsContainer.appendChild(secondLayerButton);
+
+  const arcgisLayerButton = document.createElement('button');
+  arcgisLayerButton.textContent = 'ArcGIS Katmanı';
+  arcgisLayerButton.onclick = () => {
+    model.show_arcgis_layer = !model.show_arcgis_layer;
+  };
+  controlsContainer.appendChild(arcgisLayerButton);
+
+  const styleDropdown = document.createElement('select');
+  ['Basic', 'Positron', 'Dark Matter'].forEach(style => {
+    const option = document.createElement('option');
+    option.value = style;
+    option.text = style;
+    styleDropdown.add(option);
   });
-});
+
+  styleDropdown.onchange = (event) => {
+    model.tile_url = event.target.value === 'Basic'
+      ? "https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
+      : event.target.value === 'Positron'
+        ? "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+        : "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json";
+  };
+  controlsContainer.appendChild(styleDropdown);
+
+  el.appendChild(controlsContainer);
+
+  map.on('load', () => {
+    const addLayers = () => {
+      if (!map.getSource('geojson-layer')) {
+        map.addSource('geojson-layer', {
+          type: 'geojson',
+          data: JSON.parse(model.geojson_data),
+        });
+        map.addLayer({
+          id: 'geojson-layer',
+          type: 'fill',
+          source: 'geojson-layer',
+          paint: {
+            'fill-color': '#0080ff',
+            'fill-opacity': 0.5
+          }
+        });
+      }
+
+      if (!map.getSource('geojson-layer-2')) {
+        map.addSource('geojson-layer-2', {
+          type: 'geojson',
+          data: JSON.parse(model.geojson_data_2),
+        });
+        map.addLayer({
+          id: 'geojson-layer-2',
+          type: 'fill',
+          source: 'geojson-layer-2',
+          paint: {
+            'fill-color': '#ff0000',
+            'fill-opacity': 0.5
+          }
+        });
+      }
+
+      arcgisLayerButton.onclick = () => {
+        model.show_arcgis_layer = !model.show_arcgis_layer;
+    
+        // Eğer kaynak haritaya eklenmemişse, ekleyip sonra görünürlüğü ayarlayın
+        if (model.show_arcgis_layer && !map.getSource("trailheads")) {
+            fetch("https://akomcbs.ibb.istanbul/server/rest/services/Hosted/sensor/FeatureServer/0/query?f=geojson&where=1=1&token=" + model.token)
+                .then(response => response.json())
+                .then(data => {
+                    map.addSource("trailheads", {
+                        type: "geojson",
+                        data: data,
+                    });
+    
+                    map.addLayer({
+                        id: "arcgis-layer",
+                        type: "circle",
+                        source: "trailheads",
+                        paint: {
+                            "circle-color": "#000",
+                            "circle-stroke-width": 0.5,
+                            "circle-stroke-color": "white",
+                        }
+                    });
+    
+                    // İlk eklemede, görünürlüğü kapalı yapın.
+                    map.setLayoutProperty("arcgis-layer", "visibility", model.show_arcgis_layer ? "visible" : "none");
+                })
+                .catch(error => {
+                    console.error("ArcGIS katmanı yüklenirken hata oluştu:", error);
+                });
+        } else if (map.getSource("trailheads")) {
+            // Eğer kaynak zaten eklenmişse, sadece görünürlüğü ayarla
+            map.setLayoutProperty("arcgis-layer", "visibility", model.show_arcgis_layer ? "visible" : "none");
+        }
+      };
+    
+
+      if (!map.getSource('openmaptiles')) {
+        map.addSource('openmaptiles', {
+          url: 'https://api.maptiler.com/tiles/v3/tiles.json?key=ka2CI0XYdBCZt32lmrGA',
+          type: 'vector',
+        });
+
+        map.addLayer({
+          id: '3d-buildings',
+          source: 'openmaptiles',
+          'source-layer': 'building',
+          type: 'fill-extrusion',
+          minzoom: 15,
+          paint: {
+            'fill-extrusion-color': '#aaa',
+            'fill-extrusion-height': ['interpolate', ['linear'], ['zoom'], 15, 0, 16, ['get', 'render_height']],
+            'fill-extrusion-base': ['get', 'render_min_height'],
+            'fill-extrusion-opacity': 0.6
+          }
+        });
+      }
+
+      map.setLayoutProperty('geojson-layer', 'visibility', model.show_first_layer ? 'visible' : 'none');
+      map.setLayoutProperty('geojson-layer-2', 'visibility', model.show_second_layer ? 'visible' : 'none');
+    };
+
+    addLayers();
+
+    model.on('change:tile_url', () => {
+      map.setStyle(model.tile_url);
+      map.on('styledata', () => {
+        addLayers();
+      });
+    });
+
+    window.addEventListener('resize', () => {
+      map.resize();
+    });
+
+    model.on('change:center', () => {
+      map.setCenter(model.center);
+    });
+
+    model.on('change:zoom', () => {
+      map.setZoom(model.zoom);
+    });
+
+    model.on('change:show_first_layer', () => {
+      map.setLayoutProperty('geojson-layer', 'visibility', model.show_first_layer ? 'visible' : 'none');
+    });
+
+    model.on('change:show_second_layer', () => {
+      map.setLayoutProperty('geojson-layer-2', 'visibility', model.show_second_layer ? 'visible' : 'none');
+    });
+
+    model.on('change:show_arcgis_layer', () => {
+      map.setLayoutProperty('arcgis-layer', 'visibility', model.show_arcgis_layer ? 'visible' : 'none');
+    });
+
+  });
+}
--- panelmaplibre.py
+++ panelmaplibre.py
@@ -1,5 +1,6 @@
 import param
 import panel as pn
+from pathlib import Path
 
 from panel.custom import JSComponent
 import requests
@@ -25,7 +26,7 @@ else:
     print("Error generating token:", response.json())
 
 
-url = "https://akomcbs.ibb.istanbul/server/rest/services/Hosted/sensor/FeatureServer/0"
+url = "https://akomcbs.ibb.istanbul/server/rest/services/Hosted/sensor/FeatureServer/0/query"
 params = {
     'username': username,
     'password': password,
@@ -35,16 +36,21 @@ params = {
     "f": "json"
 }
 
+# Use the token to fetch data
 response = requests.get(url, params=params)
-data = response.json()
 
-# Yanıtın "features" dizisinde olup olmadığını kontrol edin
-if "features" in data and response.status_code == 200:
-    features = data["features"]
-    for feature in features:
-        print(feature)
-else:
-    print("Beklenen formatta özellik verisi yok:", data)
+# Check if response is JSON or HTML
+try:
+    data = response.json()
+    if "features" in data:
+        features = data["features"]
+        for feature in features:
+            print(feature)
+    else:
+        print("Expected data format is not found in response:", data)
+except ValueError:
+    # If JSON parsing fails, print the text to see the HTML response
+    print("Response is not JSON. Response text:", response.text)
 
 
 class MapLibreComponent(JSComponent):
@@ -63,207 +69,8 @@ class MapLibreComponent(JSComponent):
     show_arcgis_layer = param.Boolean(default=False, doc="Show or hide the ArcGIS layer.")
     show_3d_layer = param.Boolean(default=False, doc="Show or hide the 3d layer.")
 
-    _esm = """
-    import maplibregl from 'https://cdn.jsdelivr.net/npm/maplibre-gl@4.7.1/+esm';
-    import mapboxGlArcgisFeatureserver from 'https://cdn.jsdelivr.net/npm/mapbox-gl-arcgis-featureserver@0.0.8/+esm';
+    _esm = Path("map_scripts.js")
 
-    export function render({ model, el }) {
-      const map = new maplibregl.Map({
-        container: el,
-        style: model.tile_url,
-        center: model.center,
-        zoom: model.zoom,
-        pitch: model.pitch,  // Adding pitch for 3D perspective
-        bearing: model.bearing,  // Adding bearing to rotate map
-        antialias: true
-      });
-
-      const controlsContainer = document.createElement('div');
-      controlsContainer.style.position = 'absolute';
-      controlsContainer.style.top = '10px';
-      controlsContainer.style.left = '10px';
-      controlsContainer.style.zIndex = '1';
-      controlsContainer.style.backgroundColor = 'white';
-      controlsContainer.style.padding = '2px';
-      controlsContainer.style.borderRadius = '3px';
-      //controlsContainer.style.width = '150px';
-      //controlsContainer.style.height = '50px';
-
-      const firstLayerButton = document.createElement('button');
-      firstLayerButton.textContent = 'İlk Katman';
-      firstLayerButton.onclick = () => {
-        model.show_first_layer = !model.show_first_layer;
-      };
-      controlsContainer.appendChild(firstLayerButton);
-
-      const secondLayerButton = document.createElement('button');
-      secondLayerButton.textContent = 'İlçe Sınırları';
-      secondLayerButton.onclick = () => {
-        model.show_second_layer = !model.show_second_layer;
-      };
-      controlsContainer.appendChild(secondLayerButton);
-
-      const arcgisLayerButton = document.createElement('button');
-      arcgisLayerButton.textContent = 'ArcGIS Katmanı';
-      arcgisLayerButton.onclick = () => {
-        model.show_arcgis_layer = !model.show_arcgis_layer;
-      };
-      controlsContainer.appendChild(arcgisLayerButton);
-
-      const styleDropdown = document.createElement('select');
-      ['Basic', 'Positron', 'Dark Matter'].forEach(style => {
-        const option = document.createElement('option');
-        option.value = style;
-        option.text = style;
-        styleDropdown.add(option);
-      });
-      
-      styleDropdown.onchange = (event) => {
-        model.tile_url = event.target.value === 'Basic'
-          ? "https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
-          : event.target.value === 'Positron'
-          ? "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
-          : "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json";
-      };
-      controlsContainer.appendChild(styleDropdown);
-
-      el.appendChild(controlsContainer);
-
-      map.on('load', () => {
-        const addLayers = () => {
-            if (!map.getSource('geojson-layer')) {
-                map.addSource('geojson-layer', {
-                    type: 'geojson',
-                    data: JSON.parse(model.geojson_data),
-                });
-
-                map.addLayer({
-                    id: 'geojson-layer',
-                    type: 'fill',
-                    source: 'geojson-layer',
-                    layout: {},
-                    paint: {
-                        'fill-color': '#0080ff',
-                        'fill-opacity': 0.5
-                    }
-                });
-            }
-
-            if (!map.getSource('geojson-layer-2')) {
-                map.addSource('geojson-layer-2', {
-                    type: 'geojson',
-                    data: JSON.parse(model.geojson_data_2),
-                });
-
-                map.addLayer({
-                    id: 'geojson-layer-2',
-                    type: 'fill',
-                    source: 'geojson-layer-2',
-                    layout: {},
-                    paint: {
-                        'fill-color': '#ff0000',
-                        'fill-opacity': 0.5
-                    }
-                });
-            }
-
-            if (!map.getSource('featureserver-src')) {
-                const fsSourceId = 'featureserver-src';
-                const service = new mapboxGlArcgisFeatureserver(fsSourceId, map, {
-                    url: `${model.arcgis_url}?token=${model.token}`
-                });
-
-                console.log(`${model.arcgis_url}?token=${model.token}`);
-                console.log("aa");
-
-
-                map.addLayer({
-                    'id': 'fill-lyr',
-                    'source': fsSourceId,
-                    'type': 'fill',
-                    'paint': {
-                        'fill-opacity': 0.5,
-                        'fill-color': '#B42222'
-                    }
-                });
-            }
-
-            if (!map.getSource('openmaptiles')) {
-                map.addSource('openmaptiles', {
-                  url: 'https://api.maptiler.com/tiles/v3/tiles.json?key=ka2CI0XYdBCZt32lmrGA',
-                  type: 'vector',
-                });
-
-                map.addLayer(
-                  {
-                    'id': '3d-buildings',
-                    'source': 'openmaptiles',
-                    'source-layer': 'building',
-                    'type': 'fill-extrusion',
-                    'minzoom': 15,
-                    'paint': {
-                      'fill-extrusion-color': '#aaa',
-                      'fill-extrusion-height': [
-                        'interpolate',
-                        ['linear'],
-                        ['zoom'],
-                        15,
-                        0,
-                        16,
-                        ['get', 'render_height']
-                      ],
-                      'fill-extrusion-base': ['get', 'render_min_height'],
-                      'fill-extrusion-opacity': 0.6
-                    }
-                  }
-                );
-            }
-
-            // Katmanların görünürlüğünü ayarlayın
-            map.setLayoutProperty('geojson-layer', 'visibility', model.show_first_layer ? 'visible' : 'none');
-            map.setLayoutProperty('geojson-layer-2', 'visibility', model.show_second_layer ? 'visible' : 'none');
-            map.setLayoutProperty('fill-lyr', 'visibility', model.show_arcgis_layer ? 'visible' : 'none');
-        };
-
-        // İlk katman eklenir
-        addLayers();
-
-        // Stil değişikliği yapıldığında katmanları yeniden ekleme
-        model.on('change:tile_url', () => {
-            map.setStyle(model.tile_url);
-            map.on('styledata', () => {
-                addLayers();
-            });
-        });
-
-        window.addEventListener('resize', () => {
-            map.resize();
-        });
-
-        model.on('change:center', () => {
-            map.setCenter(model.center);
-        });
-
-        model.on('change:zoom', () => {
-            map.setZoom(model.zoom);
-        });
-
-        // Katmanların görünürlüğünü izleme ve güncelleme
-        model.on('change:show_first_layer', () => {
-            map.setLayoutProperty('geojson-layer', 'visibility', model.show_first_layer ? 'visible' : 'none');
-        });
-
-        model.on('change:show_second_layer', () => {
-            map.setLayoutProperty('geojson-layer-2', 'visibility', model.show_second_layer ? 'visible' : 'none');
-        });
-
-        model.on('change:show_arcgis_layer', () => {
-            map.setLayoutProperty('fill-lyr', 'visibility', model.show_arcgis_layer ? 'visible' : 'none');
-        });
-
-    });
-    }
-    """
 
     _stylesheets = ['https://unpkg.com/maplibre-gl@^4.7.1/dist/maplibre-gl.css']
 
@@ -294,47 +101,13 @@ map_component = MapLibreComponent(
     pitch=45, 
     bearing=-17,
     sizing_mode='stretch_both',
-    min_height=500
+    min_height=500,
 )
 
-# description = pn.pane.Markdown(
-#     "## İstanbul İlçe Haritası\n\nBu harita İstanbul ilçelerinin GeoJSON verisiyle hazırlanmıştır.",
-#     sizing_mode="stretch_width"
-# )
-
-# toggle_button = pn.widgets.Toggle(name='İlk katman', button_type='primary')
-# toggle_button_2 = pn.widgets.Toggle(name='İlçe Sınırları', button_type='primary')
-# toggle_button_arcgis = pn.widgets.Toggle(name='ArcGIS Katmanı', button_type='primary')
-# map_style_dropdown = pn.widgets.Select(name='Harita Stili', options=list(map_styles.keys()), value='Basic')
-
-
-# def toggle_layer(event):
-#     map_component.show_first_layer = event.new
-
-# def toggle_layer_2(event):
-#     map_component.show_second_layer = event.new
-
-# def toggle_arcgis_layer(event):
-#     map_component.show_arcgis_layer = event.new
-
-# def update_map_style(event):
-#     map_component.tile_url = map_styles[event.new]
-
-# # butonlar
-# toggle_button.param.watch(toggle_layer, 'value')
-# toggle_button_2.param.watch(toggle_layer_2, 'value')
-# toggle_button_arcgis.param.watch(toggle_arcgis_layer, 'value')
-# map_style_dropdown.param.watch(update_map_style, 'value')
-
 pn.Column(
-    # description,
     pn.Row(
         map_component,
         sizing_mode='stretch_both'
     ),
-    # toggle_button,
-    # toggle_button_2,
-    # toggle_button_arcgis,
-    # map_style_dropdown,
     sizing_mode='stretch_both'
 ).servable()
